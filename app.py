33820853f29fff41bff502f130bcafd8a8164829
iago-rhuda <comercial.iagorhuda@gmail.com>
2022-11-22 22:12:35 -0300

    1

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import os
 
 
 app = Flask(__name__)
@@ -8,4 +9,5 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True)
