de1e9bebf2588de8419b0f7a55cf8f6f98f37caf
iago-rhuda <comercial.iagorhuda@gmail.com>
2022-12-02 18:41:15 -0300

    Netlify 1

--- functions/app.py
+++ functions/app.py
@@ -10,3 +10,5 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
+#PATH = D:\User\Documents\Iago\Programas\node_modules\npm\bin\npm-cli.js
